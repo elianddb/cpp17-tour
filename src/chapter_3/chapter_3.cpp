f8f4176f6a880b57a95bc34d303a2352166857a6
elianddb <elian.ddb@outlook.com>
2023-07-15 01:44:01 -0400

    add use of Vector class in chapter_3.cpp (3.2)

--- src/chapter_3/chapter_3.cpp
+++ src/chapter_3/chapter_3.cpp
@@ -1,3 +1,15 @@
+#include "vector/vector.hpp"
+#include <cmath>
+#include <iostream>
+
+double sqrtSum(DDB::Vector& vec)
+{
+    double sum {};
+    for (int i {}; i != vec.size(); ++i)
+        sum += std::sqrt(vec[i]);
+    return sum;
+}
+
 int main()
 {
     // 3.2 Separate Compilation
@@ -10,5 +22,9 @@ int main()
     // These features can be used to organize a program into a set of
     // semi-independent code fragments. A library is often a collection
     // of separately compiled code fragments (e.g., functions).
+    DDB::Vector vec {4};
+    for (int i {}; i != vec.size(); ++i)
+        vec[i] = 4;
+    std::cout << sqrtSum(vec) << '\n';
     // 3.2 Separate Compilation
 }
