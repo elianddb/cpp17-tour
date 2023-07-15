39aac2ddc743e50a54fe4ee2be60d59e117460cc
elianddb <elian.ddb@outlook.com>
2023-07-15 01:24:35 -0400

    add 3.2 Separate Compilation comments

--- src/chapter_3/chapter_3.cpp
+++ src/chapter_3/chapter_3.cpp
@@ -1,4 +1,14 @@
 int main()
 {
-        
+    // 3.2 Separate Compilation
+    // C++ supports the notion of separate compilation where user code
+    // sees only declarations of the types and functions used.
+    //
+    // Definitions of those types are held in separate source files and
+    // are compiled separately.
+    //
+    // These features can be used to organize a program into a set of
+    // semi-independent code fragments. A library is often a collection
+    // of separately compiled code fragments (e.g., functions).
+    // 3.2 Separate Compilation
 }
