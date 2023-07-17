15709bae458618b1ca75dd3200be08b48c5e1b49
elianddb <elian.ddb@outlook.com>
2023-07-17 00:19:08 -0400

    amend 4.2 Concrete Types comments

--- src/chapter_4/chapter_4.cpp
+++ src/chapter_4/chapter_4.cpp
@@ -1,5 +1,17 @@
 int main()
 {
     // 4.2 Concrete Types
+    // Concrete types behave just like built-in types but have their own
+    // semantics and sets of operations.
+    // For example, vectors and strings are much like built-in arrays, except
+    // they are better behaved.
+    // The defining characteristic of a concrete type is that its
+    // representation is part of its definition. In other words, the way the
+    // object is represented in memory is fixed.
+    // Therefore, if the representation changes, a user must recompile.
+    // To increase flexibility, a concrete type can keep major parts of its
+    // representation on free store (dynamic memory, heap) and access them
+    // through the part stored in the class object itself.
     // 4.2 Concrete Types
+
 }
