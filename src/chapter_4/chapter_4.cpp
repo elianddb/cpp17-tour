be4574cd77e22f450cd684e0f36e2c88abdd62a6
Elian <elian.dap@outlook.com>
2023-07-17 11:53:03 -0400

    amend 4.2.1 An Arithmetic Type comments

--- src/chapter_4/chapter_4.cpp
+++ src/chapter_4/chapter_4.cpp
@@ -1,5 +1,8 @@
 int main()
 {
+    // When we refer to the representation, we're talking about the data
+    // members of the class.
+
     // 4.2 Concrete Types
     // Concrete types behave just like built-in types but have their own
     // semantics and sets of operations.
@@ -16,6 +19,7 @@ int main()
     // 4.2 Concrete Types
 
     // 4.2.1 An Arithmetic Type
-    // 
+    // The class definition itself only contains the operations requiring
+    // access to the representation.
     // 4.2.1 An Arithmetic Type
 }
