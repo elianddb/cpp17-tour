564d6dcaa35945c7acdb0112272ec900a6d57690
elianddb <elian.ddb@outlook.com>
2023-07-18 23:18:20 -0400

    add Complex class and complementary constructors

--- src/chapter_4/complex/complex.hpp
+++ src/chapter_4/complex/complex.hpp
@@ -1,4 +1,18 @@
 #ifndef COMPLEX_HPP_INCLUDED
 #define COMPLEX_HPP_INCLUDED
-
+namespace DDB
+{
+    // 4.2.1 "classical user-defined arithmetic type"
+    class Complex
+    {
+        double re;
+        double im;
+    public:
+        Complex(double r, double i)
+            : re{r}, im{i} {}
+        Complex(double r)
+            : re {r}, im {} {}
+        Complex() : re {} : im {} {}
+    };
+}
 #endif
