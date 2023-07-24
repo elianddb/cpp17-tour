872ed67ae309ce06e236566f0dffac6bfbfd1a1e
elianddb <elian.ddb@outlook.com>
2023-07-23 23:23:03 -0400

    amend 6.2 comments; example

--- src/chapter_6/chapter_6.cpp
+++ src/chapter_6/chapter_6.cpp
@@ -3,6 +3,14 @@ int main()
    // 6.2 Parameterized Types
    // template <typename T>
    // The prefix makes a T parameter of the declaration it prefixes.
-   // Using class instead of typename is equivalent. (template <typename t>//
+   // Using class instead of typename is equivalent. (template <typename t>
+   //
+   // In combination with member functions definitions:
+   // template <typename T>
+   // Vector<T>::Vector(int s) {}
+   // We gain the ability to define Vector's as follows:
+   // Vector<char> vc {200};
+   // 
+   // 
    // 6.2 Parameterized Types     
 }
