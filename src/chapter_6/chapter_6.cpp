4c3cab4b46b6e15f4c6381d3814842598a010d8f
elianddb <elian.ddb@outlook.com>
2023-07-23 23:21:05 -0400

    amend 6.2 comments; template prefix

--- src/chapter_6/chapter_6.cpp
+++ src/chapter_6/chapter_6.cpp
@@ -1,6 +1,8 @@
 int main()
 {
    // 6.2 Parameterized Types
-   // 
+   // template <typename T>
+   // The prefix makes a T parameter of the declaration it prefixes.
+   // Using class instead of typename is equivalent. (template <typename t>//
    // 6.2 Parameterized Types     
 }
