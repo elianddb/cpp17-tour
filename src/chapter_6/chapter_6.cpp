6a524ea47379d90c28a6cb10ca0ca321ee445fa4
elianddb <elian.ddb@outlook.com>
2023-07-25 23:17:02 -0400

    add other instantiations of template class and amend 6.3 -> 6.2.1

--- src/chapter_6/chapter_6.cpp
+++ src/chapter_6/chapter_6.cpp
@@ -1,3 +1,5 @@
+#include <list>
+
 #include "vector.hpp"
 
 int main()
@@ -21,10 +23,13 @@ int main()
     // specialization. Code is generated for each instantiation used.
     // It's also type checked to be as safe as hand written code.
     DDB::V4::Vector<char> vec{4};
+    DDB::V4::Vector<std::string> vs{17};
+    DDB::V4::Vector<std::list<int>> vli{17};
+
 
     // 6.2 Parameterized Types     
 
-    // 6.3 Constrained Template Arguments (C++20)
+    // 6.2.1 Constrained Template Arguments (C++20)
     // 
-    // 6.3 Constrained Template Arguments (C++20)
+    // 6.2.1 Constrained Template Arguments (C++20)
 }
