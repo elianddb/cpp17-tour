ef9111c5e1ab61556998fd78039a53639eb9c8b1
elianddb <elian.ddb@outlook.com>
2023-08-05 23:10:05 -0400

    amend 7.2; add sum()

--- src/chapter_7/chapter_7.cpp
+++ src/chapter_7/chapter_7.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+// 7.2 Concepts (C++20)
+// sum() from 6.3.1
+template <typename Seq, typename Num>
+Num sum(Seq s, Num v)
+{
+    for (const auto& x : s)
+        v += x;
+    return v;
+}
+// 7.2 Concepts (C++20)
+
+
 int main()
 {
     std::cout << "Chapter 7\n";
