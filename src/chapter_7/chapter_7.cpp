79e84939326cc65fc4f8dba500d9ae7c54c3c6ff
elianddb <elian.ddb@outlook.com>
2023-08-05 23:29:12 -0400

    amend 7.2.1; concept implementation

--- src/chapter_7/chapter_7.cpp
+++ src/chapter_7/chapter_7.cpp
@@ -28,5 +28,22 @@ int main()
     // 7.2 Concepts (C++20)
 
     // 7.2.1 Use of Concepts
+    // Most templates are constrained templates so generated code will work
+    // properly. The type-name prefix *typename* is the least constraining,
+    // requiring only an arg be a type.
+    //
+    // To do better use concepts.
+    //      template <Sequence Seq, Number Num>
+    // Once defined the compiler can reject bad calls by looking at sum's
+    // interface only rather than it's implementation. In addition, we
+    // should be able to add elements of a sequence to a number:
+    //      template <Sequence Seq, Number Num>
+    //          requires Arithmetic<ValueType<Seq>, Num>
+    // The ValueType of a sequence is the type of the elem of a sequence.
+    // Arithmetic<x, y> is concept that specifies we can do arithmetic
+    // with numbers of type x and y. (i.e., prevents calculation for
+    // vector<string> and vector<int*> but allows vector<int> and
+    // vector<complex<double>>)
+    // 
     // 7.2.1 Use of Concepts
 }
