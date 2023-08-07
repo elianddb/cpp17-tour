185d683b123e1e5639afd9116b0d95b06366e5d2
elianddb <elian.ddb@outlook.com>
2023-08-06 21:59:24 -0400

    add advance() basic implementation

--- src/chapter_7/chapter_7.cpp
+++ src/chapter_7/chapter_7.cpp
@@ -70,6 +70,17 @@ int main()
     // 7.2.2 Concept-based Overloading
     // Once specified, we can overload templates based on their properties.
     // Consider a concise copy of advance() from the standard-library:
-    // 
+    //      template <ForwardIterator Iter>
+    //      void advance(Iter p, int n)
+    //      {
+    //          for (--n)
+    //              ++p;
+    //      }
+    //
+    //      template <RandomAccessIterator Iter, int n>
+    //      void advance(Iter p, int n)
+    //      {
+    //          p += n;
+    //      }
     // 7.2.2 Concept-based Overloading
 }
