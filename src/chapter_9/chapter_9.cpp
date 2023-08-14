68e0c8876e3742c2bf32e85d5df1e6bdbb89fbfe
Elian <elian.dap@outlook.com>
2023-08-14 17:52:20 -0400

    add subscripting example

--- src/chapter_9/chapter_9.cpp
+++ src/chapter_9/chapter_9.cpp
@@ -23,5 +23,25 @@ int main()
 	// Initialized to the character sequence "dmr@bell-labs.com"
 	std::string addr {compose("dmr", "bell-labs.com")};
 	std::cout << addr << '\n';
+	// Alternativately += is available to concatenate items to the end
+	// of a string.
+	// 
+	// In addition to = and +=, subscripting (using []) and substring
+	// operations are supported. 
+	// e.g. Subscripting
+	std::string name {"Niels Stroustrup"};
+	auto m3 = [&]()
+	{
+		std::string s {name.substr(6, 10)}; // s = Stroustrop
+		name.replace(0, 5, "nicholas");
+		name[0] = std::toupper(name[0]);
+	};
+	// In the lambda substr and replace take two main paramaters in terms
+	// of positioning. The starting index and length. Replace has the
+	// additional value argument to replace the substring. (does not need
+	// to match length).
+	m3();
+	std::cout << name << '\n';
+	
 	// 9.2 Strings
 }
