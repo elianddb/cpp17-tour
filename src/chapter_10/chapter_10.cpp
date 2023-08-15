b6590201fe438fd6200cb38c8dd924c3fe963f81
Elian <elian.dap@outlook.com>
2023-08-15 19:44:40 -0400

    add ostream obj use

--- src/chapter_10/chapter_10.cpp
+++ src/chapter_10/chapter_10.cpp
@@ -5,6 +5,15 @@ int main()
 	std::cout << "Chapter 10\n";
 
 	// 10.2 Output
+	// In <ostream>, the I/O stream library defines output for every built-in
+	// type.
 	//
+	// The operator << ("put to") is used as an output operator on obj
+	// of type ostream; cout is the standard output stream and cerr is the
+	// standard for reporting errors. By default, values written to cout are
+	// converted to a sequence of characters.
+	//		std::cout << 10;
+	//
+	// 
 	// 10.2 Output
 }
