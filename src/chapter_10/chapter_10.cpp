87b104a9e85dbdce854213a9bf586a9bc9587863
elianddb <elian.ddb@outlook.com>
2023-08-16 22:22:06 -0400

    amend 10.2 examples

--- src/chapter_10/chapter_10.cpp
+++ src/chapter_10/chapter_10.cpp
@@ -12,8 +12,17 @@ int main()
 	// of type ostream; cout is the standard output stream and cerr is the
 	// standard for reporting errors. By default, values written to cout are
 	// converted to a sequence of characters.
-	//		std::cout << 10;
 	//
+	// Places the characters 1 and 0 on the standard output stream.
+	//		std::cout << 10;
+	// The same can be done equivalently through a variable.
 	// 
+	// When outputting several items you could do it in separate lines. But most
+	// prefer to chain the items using <<
+	//		std::cout << "the value is " << 10 << '\n';
+	//
+	// A character constant is a character enclosed in single quotes.
+	// Note: char is ouputted as a character rather than a numerical value.
+	//		char c = 'c';
 	// 10.2 Output
 }
