8de1a86885e5b57cd54b8400d96ef457f4a24422
elianddb <elian.ddb@outlook.com>
2023-08-25 23:44:04 -0400

    add std::vector info

--- src/chapter_11/chapter_11.cpp
+++ src/chapter_11/chapter_11.cpp
@@ -1,7 +1,14 @@
 int main()
 {
     // 11.2 `vector`
-
+    // std::vector is one of the most useful std lib containers.
+    // A sequence of elem of a given type, contiguously stored in mem.
+    // Typical implementations of std::vector involve a handle holding 
+    // ptrs to the first elem, one-past-the-last elem, and one-past-the-last
+    // allocated space. It also holds an allocator from which std::vector
+    // can acquire and release mem.
+    //      The default alloc uses new and delete to acquire and release elem.
+    
     // 11.2 `vector`
     return 0;
 }
